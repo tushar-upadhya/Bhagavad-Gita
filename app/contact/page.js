4ecd79979bc889a257d58ac6923e45f28d0cc409
tushar-upadhya <tusharupadhyay691@gmail.com>
2024-01-12 08:53:16 +0530

    cleanUp

--- app/contact/page.js
+++ app/contact/page.js
@@ -1,5 +1,5 @@
 import { MailIcon, HomeIcon, PhoneCall } from "lucide-react";
-// components
+
 import Form from "@/components/Form";
 
 const Contact = () => {
@@ -10,23 +10,23 @@ const Contact = () => {
         <div className="grid xl:grid-cols-2 pt-12 xl:h-[480px] mb-6 xl:mb-24">
           {/* text */}
           <div className="flex flex-col justify-center">
-            <div className="flex items-center gap-x-4 text-primary text-lg mb-4">
+            <div className="flex items-center mb-4 text-lg gap-x-4 text-primary">
               <span className="w-[30px] h-[2px] bg-primary"></span>
               Say Hello 👋
             </div>
-            <h1 className="h1 max-w-md mb-8">Let's Work Together.</h1>
+            <h1 className="max-w-md mb-8 h1">Let's Work Together.</h1>
             <p className="subtitle max-w-[400px]">
               Lorem ipsum dolor, sit amet consectetur adipisicing elit. Placeat,
               nostrum.
             </p>
           </div>
           {/* illustration */}
-          <div className="hidden xl:flex w-full bg-contact_illustration_light dark:bg-contact_illustration_dark bg-contain bg-top bg-no-repeat"></div>
+          <div className="hidden w-full bg-top bg-no-repeat bg-contain xl:flex bg-contact_illustration_light dark:bg-contact_illustration_dark"></div>
         </div>
         {/* info text & form */}
-        <div className="grid xl:grid-cols-2 mb-24 xl:mb-32">
+        <div className="grid mb-24 xl:grid-cols-2 xl:mb-32">
           {/* info text */}
-          <div className="flex flex-col gap-y-4 xl:gap-y-14 mb-12 xl:mb-24 text-base xl:text-lg">
+          <div className="flex flex-col mb-12 text-base gap-y-4 xl:gap-y-14 xl:mb-24 xl:text-lg">
             {/* mail */}
             <div className="flex items-center gap-x-8">
               <MailIcon size={18} className="text-primary" />
---
+++ utils/utils.js
@@ -0,0 +1,7 @@
+import { projectData } from "./utilsData";
+
+// remove category duplicates from  project Page
+export const uniqueCategories = [
+  "all projects",
+  ...new Set(projectData.map((item) => item.category)),
+];
